0220d05c7e15a82989a5bd6d676a111e9606b3ec
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-23 02:59:29 +0530

    - added images

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import axios from '../app/service/axios'
+import React, { useEffect, useState } from 'react'
 
-const Row = ({ title }) => {
+const Row = ({
+    title,
+    fetchUrl,
+    isLargeRow = false
+}) => {
+    const [movies, setMovies] = useState([])
+    const imageUrl = 'https://image.tmdb.org/t/p/original/'
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }, [fetchUrl])
+
     return (
         <div className="row">
             <div className="row-title">
                 {title}
+            </div>
+            <div className="row-posters">
+                {
+                    movies.map((movie) => (
+                        <img
+                            className={`row-img ${isLargeRow && "row-img-large"}`}
+                            key={movie.id}
+                            src={
+                                `${imageUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                }`
+                            }
+                            alt={movie.name}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
