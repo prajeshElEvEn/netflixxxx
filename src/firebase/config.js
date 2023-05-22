fed5d872486622b78e7193a890a24f80663a9dca
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-22 17:45:55 +0530

    added fb config

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1TloZ9rDwrd6UWX9sOqbyxp3Yau8t1i8",
+  authDomain: "netflixxxx-2d005.firebaseapp.com",
+  projectId: "netflixxxx-2d005",
+  storageBucket: "netflixxxx-2d005.appspot.com",
+  messagingSenderId: "1080258592856",
+  appId: "1:1080258592856:web:5b9f1302f87cda3316ca2d",
+  measurementId: "G-CY3X8R7F47"
+};
