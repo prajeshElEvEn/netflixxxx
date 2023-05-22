c38494346d8672e4bb9c883d7ea6868de0b1d503
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-22 22:40:59 +0530

    - created pages - created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles/App.css';
+import HomeScreen from './pages/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="app">
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+export default Navbar
