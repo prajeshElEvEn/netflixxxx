b8ef365cf1d8a156676934523a808fe8643c817e
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-22 23:45:17 +0530

    - added banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import BannerImage from '../assets/images/banner.jpg'
 
 const Banner = () => {
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
     return (
-        <div>Banner</div>
+        <div
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("${BannerImage}")`,
+                backgroundPosition: "center center"
+            }}
+        >
+            <div className="banner-items">
+                <div className="banner-title">
+                    Movie Title
+                </div>
+                <div className="banner-btns">
+                    <button className="banner-btn">Play</button>
+                    <button className="banner-btn">My List</button>
+                </div>
+                <div className="banner-desc">
+                    {
+                        truncate(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque unde voluptatem quae repellendus assumenda id aperiam facere repudiandae vero. Dolorum similique recusandae reprehenderit eaque eos eum laudantium saepe laboriosam eveniet? ", 200)
+                    }
+                </div>
+            </div>
+            <div className="banner-fade" />
+        </div >
     )
 }
 
