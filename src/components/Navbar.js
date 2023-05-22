a79e421b9e3a6c74673b0bd8abd4956d7ba26ff8
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-22 23:04:00 +0530

    - added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Logo from '../assets/images/logo.png'
+import Avatar from '../assets/images/avatar.png'
 
 const Navbar = () => {
+    const [show, setShow] = useState(false)
+
+    const transitionNavbar = () => {
+        if (window.scrollY > 100) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', transitionNavbar)
+        return () => window.removeEventListener('scroll', transitionNavbar)
+    }, [])
+
+
     return (
-        <div className="navbar">
-            <img src="" alt="" />
-            <img src="" alt="" />
+        <div className={`navbar ${show && "nav-black"}`}>
+            <div className="nav-items">
+                <img src={Logo} alt="Logo" className='nav-logo' />
+                <img src={Avatar} alt="Avatar" className='nav-avatar' />
+            </div>
         </div>
     )
 }
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const HomeScreen = () => {
     return (
         <div className='home'>
-
+            <Navbar />
         </div>
     )
 }
